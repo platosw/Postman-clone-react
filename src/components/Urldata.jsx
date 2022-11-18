b276beae7806e9cd3099024f8036882e0a873a69
Jason Park <platosw83@gmail.com>
2022-11-18 15:12:56 -0500

    send url and request api data

--- src/components/Urldata.jsx
+++ src/components/Urldata.jsx
@@ -1,30 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Urldata() {
+  const selectMethod = ["GET", "POST", "PUT", "PATCH", "DELETE"];
+
+  const [method, setMethod] = useState("GET");
+  const [url, setUrl] = useState("");
+  const [data, setData] = useState();
+
+  const handleMethod = (event) => {
+    setMethod(event.target.value);
+  };
+  const handleUrl = (event) => {
+    setUrl(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    axios({
+      url: url,
+      method: method,
+      params: {},
+      headers: {},
+      data: null,
+    })
+      .catch((error) => console.log(error.message))
+      .then((response) => {
+        setData(JSON.stringify(response.data));
+      });
+  };
+
   return (
     <>
-      <form>
-        <select className="border-2">
-          <option value="GET" defaultValue>
-            GET
-          </option>
-          <option value="POST">POST</option>
-          <option value="PUT">PUT</option>
-          <option value="PATCH">PATCH</option>
-          <option value="DELETE">DELETE</option>
-        </select>
-        <input
-          className="border-2"
-          type="url"
-          placeholder="https://example.com"
-        />
-        <button
-          type="submit"
-          className="bg-blue-600 hover:bg-blue-400 text-white"
-        >
-          Send
-        </button>
+      <form data-form>
+        <div className="w-full">
+          <select onChange={handleMethod}>
+            {selectMethod.map((method) => {
+              return (
+                <option key={method} value={method}>
+                  {method}
+                </option>
+              );
+            })}
+          </select>
+          <input
+            className="w-96"
+            type="url"
+            placeholder="https://example.com"
+            value={url}
+            onChange={handleUrl}
+          />
+          <button
+            type="button"
+            className="bg-blue-600 hover:bg-blue-400 text-white"
+            onClick={handleSubmit}
+          >
+            Send
+          </button>
+        </div>
       </form>
+
+      <div>
+        <h1>Body</h1>
+        <p>{data}</p>
+      </div>
     </>
   );
 }
