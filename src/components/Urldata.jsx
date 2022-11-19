7709373fe6acbb3cb780d4bf457ba73710cdca5e
Jason Park <platosw83@gmail.com>
2022-11-19 14:14:07 -0500

    init code editor and response body

--- src/components/Urldata.jsx
+++ src/components/Urldata.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
+import CodeEditor, { SelectionText } from "@uiw/react-textarea-code-editor";
 
 export default function Urldata() {
   const selectMethod = ["GET", "POST", "PUT", "PATCH", "DELETE"];
 
   const [method, setMethod] = useState("GET");
   const [url, setUrl] = useState("");
-  const [data, setData] = useState();
+  const [datas, setDatas] = useState();
 
   const handleMethod = (event) => {
     setMethod(event.target.value);
@@ -15,17 +16,28 @@ export default function Urldata() {
     setUrl(event.target.value);
   };
 
+  const textRef = useRef();
+  const [code, setCode] = useState(`[\n\n]`);
+
+  useEffect(() => {
+    if (textRef.current) {
+      const obj = new SelectionText(textRef.current);
+      console.log("obj:", obj);
+    }
+  }, []);
+
   const handleSubmit = () => {
     axios({
       url: url,
       method: method,
       params: {},
       headers: {},
-      data: null,
+      code,
+      datas,
     })
       .catch((error) => console.log(error.message))
       .then((response) => {
-        setData(JSON.stringify(response.data));
+        setDatas(response);
       });
   };
 
@@ -56,12 +68,24 @@ export default function Urldata() {
           >
             Send
           </button>
+          <div id="editor">
+            <div data-color-mode="light" id="editor-light" className="border-2">
+              <CodeEditor
+                value={code}
+                ref={textRef}
+                language="js"
+                placeholder="Please enter JS code."
+                onChange={(evn) => setCode(evn.target.value)}
+                padding={15}
+              />
+            </div>
+          </div>
         </div>
       </form>
 
       <div>
-        <h1>Body</h1>
-        <p>{data}</p>
+        <h2 className="text-3xl">Response</h2>
+        <p>{JSON.stringify(datas)}</p>
       </div>
     </>
   );
