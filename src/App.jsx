243ffcaf1759b66e77e3aea06e39ec002608ceb0
Jason Park <platosw83@gmail.com>
2022-12-28 13:53:39 -0500

    add favicon, logo and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
 import Urldata from "./components/Urldata";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShuttleSpace } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   return (
     <>
+      <div className="logo-container">
+        <FontAwesomeIcon icon={faShuttleSpace} className="logo" />
+      </div>
+      <h1 className="title-container">POSTSHUTTLE</h1>
       <Urldata />
     </>
   );
